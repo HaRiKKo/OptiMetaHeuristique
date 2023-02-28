eee9fb98fa21e503dc85e31dc5a7ce02d4aac46b
chleo <chevrierch@eisti.eu>
2023-02-28 19:51:35 +0100

    compute makespan et disponibilité & autres

--- NSGAII.py
+++ NSGAII.py
@@ -3,6 +3,10 @@ import random
 import numpy as np
 import pandas as pd
 
+# Info sur le model
+nb_tache = 5
+nb_VM = 3
+
 # Ressources paths
 task_connectivity_path = './ressources/Task_connectivity_5.csv'
 task_instructions_path = './ressources/Task_instructions_5.csv'
@@ -40,7 +44,7 @@ def random_solution(nb_tache, nb_vm):
 def create_start_population(nb_pop=10):
     starter=[]
     for i in range(nb_pop):
-        starter.append(random_solution(5,3))
+        starter.append(random_solution(nb_tache, nb_VM))
     return starter
 
 starter=create_start_population()
@@ -62,7 +66,7 @@ def compute_temps_execution(task_id, VM_id):
     
     return (nb_instruction/VM_instruction_per_sec)
 
-# compute_temps_execution : calcule le temps d'execution d'une tache sur une VM
+# compute_cout_execution : calcule le cout d'execution d'une tache sur une VM
 #                           Cout d'execution en €$£
 def compute_cout_execution(task_id, VM_id):
 
@@ -75,6 +79,18 @@ def compute_cout_execution(task_id, VM_id):
     
     return (nb_instruction/VM_instruction_per_sec) * VM_cost_per_sec
 
+def compute_cout_transfert(solution, tache_source, tache_dest):
+    VM_source = solution[tache_source]
+    VM_dest = solution[tache_dest]
+    
+    data_tranfert = task_connectivity[tache_dest][tache_source]
+    cout_communication = VM_cost[VM_dest][VM_source]
+
+    return data_tranfert * cout_communication
+
+
+# compute_date_fin : calcule la date de fin d'une tache = le temps entre le début du traitement de la premier tache et la fin de la tache cible 
+#                   date de fin en seconde
 def compute_date_fin(solution, task_id):
     # Récupération de l'id de la VM pour la tâche task_id
     VM_id = solution[task_id]
@@ -83,15 +99,30 @@ def compute_date_fin(solution, task_id):
     for previous in range(len(solution)):
         if task_connectivity[task_id][previous] !=0 :
             previous_connected_tasks.append(previous)
+
     return compute_temps_execution(task_id, VM_id) + max([0]+[compute_date_fin(solution, task) for task in previous_connected_tasks])
-    
+
+"""
 test = starter[0]
-#print("solution : ",test)
-#print(compute_temps_execution(0,test[0]))
+print("solution : ",test)
+print(compute_temps_execution(0,test[0]))
 
 for i in range(5):
      print("current task id:",i)
      print("date fin:")
      print(compute_date_fin(test,i))
+"""
 
+def compute_makespan(solution):
+    df_list=[compute_date_fin(solution, tache_id) for tache_id in solution.keys()]
+    
+    return max(df_list)
 
+#print(compute_makespan(starter[0]))
+
+def compute_disponibilite(solution):
+    somme = 0
+    for key in solution.keys():
+        somme += compute_temps_execution(key, solution[key])
+
+        return 1/nb_VM * (1-somme/compute_makespan(solution))

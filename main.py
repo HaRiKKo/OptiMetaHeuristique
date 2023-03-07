e09e9483ef8034f2ea2b27df56c6ae4b1c974789
benmessao <celia.benmessaouda@gmail.com>
2023-03-07 20:51:32 +0100

    correct compute_disponibilite

--- main.py
+++ main.py
@@ -8,16 +8,21 @@ from src.functions import *
 from src.utils import *
 
 import warnings
-warnings.filterwarnings('ignore')
 
-sys.path.insert(0, os.path.dirname(os.path.abspath('constants')))
+warnings.filterwarnings("ignore")
+
+sys.path.insert(0, os.path.dirname(os.path.abspath("constants")))
 from constants.parse_parameters import *
 
 file_path = read_files(resource_path)
-task_connectivity, task_instructions, VM_caracteristique, VM_cost = read_resources(file_path)
+task_connectivity, task_instructions, VM_caracteristique, VM_cost = read_resources(
+    file_path
+)
 
 starter = create_start_population()
 
-score = evaluate_population(starter)
+# score = evaluate_population(starter)
 
-ranking(score)
+# ranking(score)
+
+print(starter[0], "\n", compute_disponibilite(starter[0]))
--- src/compute_objectives.py
+++ src/compute_objectives.py
@@ -4,11 +4,14 @@ import sys
 import numpy as np
 
 from src.utils import *
-sys.path.insert(0, os.path.dirname(os.path.abspath('constants')))
+
+sys.path.insert(0, os.path.dirname(os.path.abspath("constants")))
 from constants.parse_parameters import *
 
 file_path = read_files(resource_path)
-task_connectivity, task_instructions, VM_caracteristique, VM_cost = read_resources(file_path)
+task_connectivity, task_instructions, VM_caracteristique, VM_cost = read_resources(
+    file_path
+)
 
 # Info sur le model
 nb_tache = 5
@@ -23,8 +26,9 @@ def compute_temps_execution(task_id, VM_id):
     nb_instruction = task_instructions[task_id]
     # Nombre d'instruction par seconde que la VM peut traiter (#/s)
     VM_instruction_per_sec = VM_caracteristique[VM_id][1]
-    
-    return (nb_instruction/VM_instruction_per_sec)
+
+    return nb_instruction / VM_instruction_per_sec
+
 
 # compute_cout_execution : calcule le cout d'execution d'une tache sur une VM
 #                           Cout d'execution en €$£
@@ -36,22 +40,23 @@ def compute_cout_execution(task_id, VM_id):
     VM_instruction_per_sec = VM_caracteristique[VM_id][1]
     # Coût par seconde de la VM (€/s)
     VM_cost_per_sec = VM_caracteristique[VM_id][0]
-    
-    return (nb_instruction/VM_instruction_per_sec) * VM_cost_per_sec
+
+    return (nb_instruction / VM_instruction_per_sec) * VM_cost_per_sec
+
 
 # compute_cout_execution : calcule le cout de transfert pour le partage des donnée d'une VM à une autres entre deux taches
 #                           Cout de transferts en €$£
 def compute_cout_transfert(solution, tache_source, tache_dest):
     VM_source = solution[tache_source]
     VM_dest = solution[tache_dest]
-    
+
     data_tranfert = task_connectivity[tache_dest][tache_source]
     cout_communication = VM_cost[VM_dest][VM_source]
 
     return data_tranfert * cout_communication
 
 
-# compute_date_fin : calcule la date de fin d'une tache = le temps entre le début du traitement de la premier tache et la fin de la tache cible 
+# compute_date_fin : calcule la date de fin d'une tache = le temps entre le début du traitement de la premier tache et la fin de la tache cible
 #                   date de fin en secondes
 def compute_date_fin(solution, task_id):
     # Récupération de l'id de la VM pour la tâche task_id
@@ -59,67 +64,76 @@ def compute_date_fin(solution, task_id):
     # Recherche des tâches précédentes
     previous_connected_tasks = []
     for previous in range(len(solution)):
-        if task_connectivity[task_id][previous] !=0 :
+        if task_connectivity[task_id][previous] != 0:
             previous_connected_tasks.append(previous)
 
-    return compute_temps_execution(task_id, VM_id) + max([0]+[compute_date_fin(solution, task) for task in previous_connected_tasks])
+    return compute_temps_execution(task_id, VM_id) + max(
+        [0] + [compute_date_fin(solution, task) for task in previous_connected_tasks]
+    )
+
 
 # compute_makespan : temps de completion ou temps d'execution de tout le workflow = date de fin maximum
 #                   en secondes
 def compute_makespan(solution):
-    df_list=[compute_date_fin(solution, tache_id) for tache_id in solution.keys()]
-    
-    #print("makespan", max(df_list))
+    df_list = [compute_date_fin(solution, tache_id) for tache_id in solution.keys()]
+
+    # print("makespan", max(df_list))
     return max(df_list)
 
-#print("makespan",compute_makespan(starter[0]))
 
-# compute_temps_VM : temps d'actvité d'une VM 
-#                   en secondes 
-#hypothèse : le temps d'activation d'une VM = DF(dernière tache) - DF(première tache) + temps execution(première tache)
+# print("makespan",compute_makespan(starter[0]))
+
+# compute_temps_VM : temps d'actvité d'une VM
+#                   en secondes
+# hypothèse : le temps d'activation d'une VM = DF(dernière tache) - DF(première tache) + temps execution(première tache)
 def compute_temps_VM(solution, VM_id):
-    tasks_id=[k for k, v in solution.items() if v == VM_id]
-    #print(tasks_id)
-    if len(tasks_id)>0:
-        temps_VM = compute_date_fin(solution, tasks_id[-1]) - compute_date_fin(solution, tasks_id[0]) + compute_temps_execution(tasks_id[0], VM_id)
+    tasks_id = [k for k, v in solution.items() if v == VM_id]
+    # print(tasks_id)
+    if len(tasks_id) > 0:
+        temps_VM = (
+            compute_date_fin(solution, tasks_id[-1])
+            - compute_date_fin(solution, tasks_id[0])
+            + compute_temps_execution(tasks_id[0], VM_id)
+        )
         return temps_VM
     else:
         return 0
 
-"""NE MARCHE PAS"""
+
 # compute_disponibilité : représente utilisation des VM dans le workflows
 def compute_disponibilite(solution):
     somme = 0
     makespan = compute_makespan(solution)
     for vm in range(nb_VM):
-        #somme += compute_temps_execution(key, solution[key])
-        somme += compute_temps_VM(solution, vm)/makespan
-    #print(somme)
-    #print(compute_makespan(solution))
+        # somme += compute_temps_execution(key, solution[key])
+        somme += (-1) * compute_temps_VM(solution, vm) / makespan
+    # print(somme)
+    # print(compute_makespan(solution))
 
-    #print("disponibilité", 1/nb_VM * (1-somme))
-    return 1/nb_VM * (1-somme)
+    # print("disponibilité", 1/nb_VM * (1-somme))
+    return (1 - somme) / nb_VM
 
-#print("disponibilité", compute_disponibilite(starter[0]))
+
+# print("disponibilité", compute_disponibilite(starter[0]))
 # compute_cout : cout total du workflow = cout execution+cout tranferts
 def compute_cout(solution):
     # compute le cout d'execution de la solution
     cout_execution = 0
     for key in solution.keys():
         cout_execution += compute_cout_execution(key, solution[key])
-    #print("cout d'execution :",cout_execution)
+    # print("cout d'execution :",cout_execution)
 
     # compute le cout de tranfers de la solution
     cout_transfert = 0
     for idx, i in np.ndenumerate(task_connectivity):
-        #print(idx,i)
+        # print(idx,i)
         if i != 0:
-            #print(idx[0], idx[1])
+            # print(idx[0], idx[1])
             cout_transfert += compute_cout_transfert(solution, idx[0], idx[1])
-    #print("cout de transfert",cout_transfert)
+    # print("cout de transfert",cout_transfert)
 
-    #print("cout total", cout_execution+cout_transfert)
-    return cout_execution+cout_transfert
+    # print("cout total", cout_execution+cout_transfert)
+    return cout_execution + cout_transfert
 
-#print("cout",compute_cout(starter[0]))
 
+# print("cout",compute_cout(starter[0]))
